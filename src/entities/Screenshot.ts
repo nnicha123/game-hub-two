c5d949a615963a984a3f998551093172650b6cf9
nnicha123 <nicha_ngamtweerat@hotmail.com>
2024-10-17 15:16:28 +0700

    Refactor entity interfaces

--- src/entities/Screenshot.ts
+++ src/entities/Screenshot.ts
@@ -1,9 +1,9 @@
-
-
-export interface Screenshot {
+interface Screenshot {
   height: number;
   id: number;
   image: string;
   is_deleted: boolean;
   width: number;
 }
+
+export default Screenshot;
